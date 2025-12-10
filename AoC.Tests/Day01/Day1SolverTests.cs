c2ec3bd2e952637a0aade6caa844273c64e7f1cb
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2025-12-10 01:42:06 +0000

    Day 1 part 2 done

--- AoC.Tests/Day01/Day1SolverTests.cs
+++ AoC.Tests/Day01/Day1SolverTests.cs
@@ -46,7 +46,7 @@ public class Day1SolverTests
         var part2ExampleResult = _sut.SolvePart2(ExampleInput);
 
         // ASSERT
-        part2ExampleResult.Should().Be(null);
+        part2ExampleResult.Should().Be(6);
     }
 
     [Test]
@@ -56,6 +56,6 @@ public class Day1SolverTests
         var part2Result = _sut.SolvePart2();
 
         // ASSERT
-        part2Result.Should().Be(null);
+        part2Result.Should().Be(6386);
     }
 }
--- AoC/Day01/Day1Solver.cs
+++ AoC/Day01/Day1Solver.cs
@@ -9,7 +9,7 @@ public class Day1Solver : ISolver
         var dialPointer = 50;
         var numOfZeros = 0;
 
-        foreach (var (dir, amount) in input.ReadLines().Select(line => (Dir: line[0] == 'L' ? -1 : 1, Amount: int.Parse(line[1..]))))
+        foreach (var (dir, amount) in ParseInput(input))
         {
             dialPointer += dir * amount;
             dialPointer %= 100;
@@ -25,6 +25,25 @@ public class Day1Solver : ISolver
 
     public long? SolvePart2(string input)
     {
-        return null;
+        var dialPointer = 50;
+        var numOfZeros = 0;
+
+        foreach (var (dir, amount) in ParseInput(input))
+        {
+            for (var click = 0; click < amount; click++)
+            {
+                dialPointer += dir;
+
+                if (dialPointer % 100 == 0)
+                {
+                    numOfZeros++;
+                }
+            }
+        }
+
+        return numOfZeros;
     }
+
+    static IEnumerable<(int Dir, int Amount)> ParseInput(string input) =>
+        input.ReadLines().Select(line => (Dir: line[0] == 'L' ? -1 : 1, Amount: int.Parse(line[1..])));
 }
